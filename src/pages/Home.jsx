0f38b6eb8b8f34d8c9bf8536cd4ca85c5122aeae
BSlaven <slaven.bunijevac@gmail.com>
2024-10-08 15:05:41 +0200

    add links on  home page to go to create, join, and rules

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+      <h1>Dobrodošli u igru država i gradova</h1>
+      <h3>Kreirajte novu igru, ili se pridružite postojećoj</h3>
+      <ul>
+        <Link to='/create'>Nova igra</Link>
+        <Link to='/join'>Pridruži se</Link>
+        <Link to='/rules'>Pravila igre</Link>
+      </ul>
+    </main>
   )
 }
 
