e039bd1393a7c778cb656835aeb84c3c47d3d2da
BSlaven <slaven.bunijevac@gmail.com>
2024-10-24 17:35:32 +0200

    add enter room page elements

--- src/pages/JoinRoom.jsx
+++ src/pages/JoinRoom.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react"
+
 const JoinRoom = () => {
+
+  const [ roomId, setRoomId ] = useState('');
+
+  const roomIdChangeHandler = e => {
+    const roomValue = e.target.value;
+    setRoomId(roomValue);
+  }
+  
   return (
-    <div>JoinRoom</div>
+    <div className="container">
+      <label htmlFor="room">Enter room number</label>
+      <input 
+        type="text"
+        id="room"
+        name="room" 
+        placeholder="Room number"
+        className="room_number_input"
+        onChange={roomIdChangeHandler}
+        value={roomId}
+      />
+      <button className="room_no_btn">Confirm</button>
+    </div>
   )
 }
 
