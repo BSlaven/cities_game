e123d46eae31d27d9bec01881c56c7df5b2b055a
BSlaven <slaven.bunijevac@gmail.com>
2024-11-11 23:28:34 +0100

    add checkbox on change handler

--- src/pages/CreateNew.jsx
+++ src/pages/CreateNew.jsx
@@ -1,35 +1,55 @@
+import { useState, useEffect } from "react";
+
 const CreateNew = () => {
+  
+  const [ selectedFields, setSelectedFields ] = useState([]);
+
+  useEffect(() => {
+    console.log(selectedFields);
+  }, [selectedFields]);
+
+  const fieldCheckHanlder = e => {
+    const fieldName = e.target.name;
+    if(selectedFields.includes(fieldName)) {
+      console.log('ima ga')
+      setSelectedFields(selectedFields.filter(field => field !== fieldName));
+      return;
+    }
+
+    setSelectedFields(prevItems => [ ...prevItems, fieldName ])
+  }
+  
   return (
     <div className="container">
       <div className="fields-container">
         <h4 className="fields-title">Opcije</h4>
         <div className="input-container">
           <label htmlFor="state-field">Države</label>
-          <input type="radio" name="state-field" id="state-field" />
+          <input onChange={fieldCheckHanlder} type="checkbox" name="state-field" id="state-field" />
         </div>
         <div className="input-container">
           <label htmlFor="cities-field">Gradovi</label>
-          <input type="radio" name="cities-field" id="cities-field" />
+          <input onChange={fieldCheckHanlder} type="checkbox" name="cities-field" id="cities-field" />
         </div>
         <div className="input-container">
           <label htmlFor="rivers-field">Rijeke</label>
-          <input type="radio" name="rivers-field" id="rivers-field" />
+          <input onChange={fieldCheckHanlder} type="checkbox" name="rivers-field" id="rivers-field" />
         </div>
         <div className="input-container">
           <label htmlFor="mountains-field">Planine</label>
-          <input type="radio" name="mountains-field" id="mountains-field" />
+          <input onChange={fieldCheckHanlder} type="checkbox" name="mountains-field" id="mountains-field" />
         </div>
         <div className="input-container">
           <label htmlFor="names-field">Imena</label>
-          <input type="radio" name="names-field" id="names-field" />
+          <input onChange={fieldCheckHanlder} type="checkbox" name="names-field" id="names-field" />
         </div>
         <div className="input-container">
           <label htmlFor="vegefruit-field">Voće i povrće</label>
-          <input type="radio" name="vegefruit-field" id="vegefruit-field" />
+          <input onChange={fieldCheckHanlder} type="checkbox" name="vegefruit-field" id="vegefruit-field" />
         </div>
         <div className="input-container">
           <label htmlFor="animals-field">Životinje</label>
-          <input type="radio" name="animals-field" id="animals-field" />
+          <input onChange={fieldCheckHanlder} type="checkbox" name="animals-field" id="animals-field" />
         </div>
       </div>
       <div className="time-options-container">
